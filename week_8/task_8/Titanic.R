8c97cfca0d087117a346d08af79826ad6dec5569
dppss90008 <34399929+dppss90008@users.noreply.github.com>
2018-05-01 21:50:49 +0800

    RRRR

--- week_8/task_8/Titanic.R
+++ week_8/task_8/Titanic.R
@@ -1,13 +1,41 @@
+library(magrittr)
+library(ggplot2)
 # Read the titanicTrain data and store it in titanic
 titanic <- read.csv("titanicTrain.csv")
+titanic <- titanic[c(1:1000),] 
 # Train data 上面的NA
 str(titanic)
+sapply(titanic, function(x) {sum(is.na(x))})
 
+# 篩選登船與沒有登船存活率
 boat <- titanic[titanic$boat!='',]
 notboat <- titanic[titanic$boat=='',]
-Summary(boat$survived)
+boat <- summary(as.factor(boat$survived))
+notboat <- summary(as.factor(notboat$survived))
+names(boat) <- c("No","Yes")
+boat %>% barplot
 
-boat$survived
-summary(as.factor(boat$survived))
-summary(as.factor(notboat$survived))
+# 篩選男女存活率
+male <- titanic[titanic$sex=='male',]
+female <- titanic[titanic$sex=='female',]
+summary(as.factor(male$survived))
+summary(as.factor(female$survived))
+
+# Pclass 
+p1 <- titanic[titanic$pclass==1,] 
+summary(as.factor(p1$survived))
+p2 <- titanic[titanic$pclass==2,]
+summary(as.factor(p2$survived))
+p3 <- titanic[titanic$pclass==3,]
+summary(as.factor(p3$survived))
+
+# embarked
+C <- titanic[titanic$embarked=="C",] 
+summary(as.factor(C$survived))
+Q <- titanic[titanic$embarked=="Q",] 
+summary(as.factor(Q$survived))
+S <- titanic[titanic$embarked=="S",] 
+summary(as.factor(S$survived))
+empty <- titanic[titanic$embarked=="",] 
+summary(as.factor(empty$survived))
 
